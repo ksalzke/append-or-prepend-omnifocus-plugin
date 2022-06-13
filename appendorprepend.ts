f9d1e00239c086f756027e28fc6231913966d913
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-06-13 17:07:19 +1000

    refactor: :recycle: update Typescript, not just JS, file

--- appendorprepend.ts
+++ appendorprepend.ts
@@ -3,7 +3,7 @@
 "targets": ["omnifocus"],
 "author": "Kaitlin Salzke",
 "identifier": "com.KaitlinSalzke.AppendOrPrepend",
-"version": "1.1",
+"version": "1.2.0",
 "description": "Append or prepend text to the name of the selected task(s)",
 "label": "Append or Prepend",
 "shortLabel": "Append or Prepend",
@@ -11,6 +11,7 @@
 }*/
 (() => {
   const action: PlugIn.Action = new PlugIn.Action(async (selection: any) => {
+    const tasks: Array<Task> = [...selection.tasks, ...selection.projects.map((p: Project) => p.task)]
     // build form
     const inputForm: any = new Form()
     inputForm.addField(new Form.Field.String('textInput', 'Text To Append/Prepend', null, null), null)
@@ -20,11 +21,11 @@
     // get form values and adjust task name(s)
     const textValue: string = inputForm.values.textInput
     const prepend: boolean = inputForm.values.prependSwitch
-    selection.tasks.forEach((task: Task) => task.name = prepend ? task.name = textValue + task.name : task.name + textValue)
+    tasks.forEach((task: Task) => task.name = prepend ? task.name = textValue + task.name : task.name + textValue)
   })
 
   action.validate = (selection: any) => {
-    return selection.tasks.length > 0
+    return selection.tasks.length + selection.projects.length > 0;
   }
 
   return action
